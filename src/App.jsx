beda1409a7484f05eb488c11263f3265f7e5bd8b
Daman <langdaman716@gmil.com>
2025-05-23 03:26:47 +0530

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "./App.css";
 import { useState } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { useEffect } from "react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{""}
+      <div className={`min-h-screen transiton-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </div>
     </>
   )
 }
